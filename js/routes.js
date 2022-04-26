18afe0103f8ebdcb3af7a2da6a02c0f7e6cae745
KillJoy <elennajaurgui@gmail.com>
2022-04-26 14:55:32 -0500

    Nuevo diseño para considerar al integrar react

---
+++ js/routes.js
@@ -0,0 +1,38 @@
+const PATHS ={
+    about:{
+        path: '/about',
+        template: `<h2>Acerca de nosotros</h2>
+        <p>Tenemos el proposito de conectar la tecnologia web con la naturaleza, concentrando a los mejores
+         productores de calidad y precio del mercado ecofliendly. Nos apollamos de una correcta y renovadora
+         difusion de nuestra filsofia mediante murales, jornadas de limpieza sin grandes fines de lucro 
+         unicamente el de nuestros trabajadores. </p>`
+  
+    },
+    beauty:{
+        path: '/beauty',
+        template: `<h2>Acerca de nosotros</h2>
+          <p>Tenemos el proposito de conectar la tecnologia web con la naturaleza, concentrando a los mejores
+           productores de calidad y precio del mercado ecofliendly. Nos apollamos de una correcta y renovadora
+           difusion de nuestra filsofia mediante murales, jornadas de limpieza sin grandes fines de lucro 
+           unicamente el de nuestros trabajadores. </p>`
+    },
+    modecofriendly:{
+      path: '/mode',
+      template: `<h2>Acerca de nosotros</h2>
+      <p>Tenemos el proposito de conectar la tecnologia web con la naturaleza, concentrando a los mejores
+       productores de calidad y precio del mercado ecofliendly. Nos apollamos de una correcta y renovadora
+       difusion de nuestra filsofia mediante murales, jornadas de limpieza sin grandes fines de lucro 
+       unicamente el de nuestros trabajadores. </p>`
+    },
+    sidesecofrienly:{
+      path: '/sides',
+      template: `<h2>Acerca de nosotros</h2>
+      <p>Tenemos el proposito de conectar la tecnologia web con la naturaleza, concentrando a los mejores
+       productores de calidad y precio del mercado ecofliendly. Nos apollamos de una correcta y renovadora
+       difusion de nuestra filsofia mediante murales, jornadas de limpieza sin grandes fines de lucro 
+       unicamente el de nuestros trabajadores. </p>`
+  
+    }
+    
+  
+  }
--- main.js
+++
@@ -1,8 +0,0 @@
-document.querySelector(".menu-btn").addEventListener("click", () => {
-    document.querySelector(".menu").classList.toggle("show");
-  });
-  
-  ScrollReveal().reveal('.showcase');
-  ScrollReveal().reveal('.news-cards', { delay: 500 });
-  ScrollReveal().reveal('.sobreRWitch', { delay: 500 });
-  ScrollReveal().reveal('.cards-banner-two', { delay: 500 });
